f9dc7add59ba69c21df2b0b4ad45a06ea83d5c94
bilal-tkxel-do <bilal.mushtaq@tkxel.com>
2024-12-31 14:53:40 +0500

    remove versioning

--- terraform/create-state-bucket.sh
+++ terraform/create-state-bucket.sh
@@ -10,24 +10,6 @@ aws s3api create-bucket \
   --bucket "$S3_BUCKET_NAME" \
   --region "$REGION"
 
-# Enable versioning on the S3 Bucket
-echo "Enabling versioning on S3 Bucket..."
-aws s3api put-bucket-versioning \
-  --bucket "$S3_BUCKET_NAME" \
-  --versioning-configuration Status=Enabled
-
-# Apply default encryption to the S3 Bucket
-echo "Applying default encryption to S3 Bucket..."
-aws s3api put-bucket-encryption \
-  --bucket "$S3_BUCKET_NAME" \
-  --server-side-encryption-configuration '{
-    "Rules": [{
-      "ApplyServerSideEncryptionByDefault": {
-        "SSEAlgorithm": "AES256"
-      }
-    }]
-  }'
-
 echo "S3 Bucket for Terraform state created successfully!"
 echo "Bucket Name: $S3_BUCKET_NAME"
 echo "Region: $REGION"
